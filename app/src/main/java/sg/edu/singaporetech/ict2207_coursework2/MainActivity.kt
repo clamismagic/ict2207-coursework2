5684516598da0bbd8815bf0d34cb66578f5bbc47
clamismagic <clamismagic@gmail.com>
2022-03-02 20:18:04 +0800

    creation of project in Android Studio

---
+++ app/src/main/java/sg/edu/singaporetech/ict2207_coursework2/MainActivity.kt
@@ -0,0 +1,11 @@
+package sg.edu.singaporetech.ict2207_coursework2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
