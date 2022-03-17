57ddf5a3d824f5142a90ab3a669f1a44f5d9e083
clamismagic <clamismagic@gmail.com>
2022-03-17 22:49:27 +0800

    added decompile code, untested

--- obfuscator_scripts/controller.py
+++ obfuscator_scripts/controller.py
@@ -1,5 +1,7 @@
 # imports here
 import os
+import subprocess
+from typing import List, Union
 
 
 class Controller:
@@ -13,8 +15,6 @@ class Controller:
 
     def __init__(self,
                  apk_path: str,
-                 working_dir_path: str = None,
-                 output_apk_path: str = None,
                  keystore_file: str = None,
                  keystore_passwd: str = None,
                  key_alias: str = None,
@@ -24,38 +24,28 @@ class Controller:
         handle inputs by user for file locations
 
         :param apk_path: string that represents the full path to the APK to be obfuscated
-        :param working_dir_path: string that represents the custom working directory to write obfuscated files
-        :param output_apk_path: string that represents the full path to output the recompiled APK after obfuscation
         :param keystore_file: string that represents the full path to the keystore file used to sign the application
         :param keystore_passwd: string that represents the password mapped to the user's specified keystore file
         :param key_alias: string that represents the key alias used for the specified keystore file
         :param key_passwd: string that represents the password mapped to the specified key alias
         """
         self.apk_path: str = apk_path
-        self.working_dir_path: str = working_dir_path
-        self.output_apk_path: str = output_apk_path
+        self.working_dir_path: str = os.path.join(os.path.dirname(self.apk_path), "decompiled_files")
+        self.output_apk_path: str = os.path.join(os.path.dirname(self.apk_path), "output_files/",
+                                                 os.path.splitext(os.path.basename(self.apk_path))[0],
+                                                 "_obfuscated.apk")
         self.keystore_file: str = keystore_file
         self.keystore_passwd: str = keystore_passwd
         self.key_alias: str = key_alias
         self.key_passwd: str = key_passwd
+        self.manifest_file: Union[str, None] = None
+        self.smali_files: List[str] = []
+        self.native_lib_files: List[str] = []
 
         # check if specified APK is valid
         if not os.path.isfile(self.apk_path):
             print('Unable to find file "{0}"'.format(self.apk_path))
             raise FileNotFoundError('Unable to find file "{0}"'.format(self.apk_path))
-
-        # if custom working directory is not specified, create a new directory "obfuscated_files"
-        # in the same directory as the APK file
-        if not self.working_dir_path:
-            self.working_dir_path = os.path.join(
-                os.path.dirname(self.apk_path), "obfuscated_files"
-            )
-            print(
-                "No working directory provided, the operations will take place in the "
-                'same directory as the input file, in the directory "{0}"'.format(
-                    self.working_dir_path
-                )
-            )
 
         if not os.path.isdir(self.working_dir_path):
             try:
@@ -68,18 +58,96 @@ class Controller:
                 )
                 raise
 
-        # if output path not specified, save in working directory
-        if not self.output_apk_path:
-            self.obfuscated_apk_path = "{0}_obfuscated.apk".format(
-                os.path.join(
-                    self.working_dir_path,
-                    os.path.splitext(os.path.basename(self.apk_path))[0],
-                )
-            )
-            print(
-                "No obfuscated apk path provided, the result will be saved "
-                'as "{0}"'.format(self.obfuscated_apk_path)
-            )
+        # decompile apk into smali
+        self.decompile_apk()
 
         return
 
+    def decompile_apk(self):
+        # The input apk will be decoded with apktool
+        apktools_path = "../tools/apktool.jar"
+        cmd = "java -jar {0} d {1} -o {2}".format(apktools_path, self.apk_path, self.working_dir_path)
+
+        try:
+            print('Running decode command "{0}"'.format(cmd))
+
+            # A new line character is sent as input since newer versions of Apktool
+            # have an interactive prompt on Windows where the user should press a key.
+            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, input=b"\n").strip()
+
+            if b"Exception in thread " in output:
+                # Report exception raised in Apktool.
+                raise subprocess.CalledProcessError(1, cmd, output)
+
+            # Path to the decoded manifest file.
+            self.manifest_file = os.path.join(
+                self.working_dir_path, "AndroidManifest.xml"
+            )
+
+            # generate a list containing the paths to all the relevant smali files obtained with apktool
+            self.smali_files = [
+                os.path.join(root, file_name)
+                for root, dir_names, file_names in os.walk(self.working_dir_path)
+                for file_name in file_names
+                if file_name.endswith(".smali")
+            ]
+
+            # remove known libraries to only grab smali files directly representing the main app
+            with open("../tools/libs_to_ignore.txt", "r", encoding="utf-8") as file:
+                read_libs_to_ignore = list(filter(None, (line.rstrip() for line in file)))
+
+                # Normalize paths for the current OS ('.join(x, "")' is used to add a trailing slash).
+            libs_to_ignore = list(
+                map(
+                    lambda x: os.path.join(os.path.normpath(x), ""),
+                    read_libs_to_ignore,
+                )
+            )
+
+            filtered_smali_files = []
+
+            for smali_file in self.smali_files:
+                # Get the path without the initial part <root>/smali/.
+                relative_smali_file = os.path.join(
+                    *(
+                        os.path.relpath(
+                            smali_file, self.working_dir_path
+                        ).split(os.path.sep)[1:]
+                    )
+                )
+                # Get only the smali files that are not part of known third
+                # party libraries.
+                if not any(
+                        relative_smali_file.startswith(lib)
+                        for lib in libs_to_ignore
+                ):
+                    filtered_smali_files.append(smali_file)
+
+            self.smali_files = filtered_smali_files
+
+            # Sort the list of smali files to always have the list in the same order.
+            self.smali_files.sort()
+
+            # A list containing the paths to the native libraries included in the application.
+            self.native_lib_files = [
+                os.path.join(root, file_name)
+                for root, dir_names, file_names in os.walk(
+                    os.path.join(self.working_dir_path, "lib")
+                )
+                for file_name in file_names
+                if file_name.endswith(".so")
+            ]
+
+            # Sort the list of native libraries to always have the list in the same order.
+            self.native_lib_files.sort()
+            return
+        except subprocess.CalledProcessError as e:
+            print(
+                "Error during decode command: {0}".format(
+                    e.output.decode(errors="replace") if e.output else e
+                )
+            )
+            raise
+        except Exception as e:
+            print("Error: {0}".format(e))
+            raise
