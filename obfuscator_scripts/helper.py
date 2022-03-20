5b364b0536186810c6e2679e36b4bf9dd05b2d89
Vexasoe <vexasoe@gmail.com>
2022-03-20 16:52:41 +0800

    Added code for NOP

---
+++ obfuscator_scripts/helper.py
@@ -0,0 +1,50 @@
+import itertools
+import logging
+import os
+import random
+import re
+import string
+from contextlib import contextmanager
+from hashlib import md5, sha256
+from typing import List
+
+#pip3 install tqdm
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+
+def show_list_progress(
+    the_list: list,
+    interactive: bool = False,
+    unit: str = "file",
+    description: str = None,
+):
+    if not interactive:
+        return the_list
+    else:
+        return tqdm(
+            the_list,
+            dynamic_ncols=True,
+            unit=unit,
+            desc=description,
+            bar_format="{l_bar}{bar}|[{elapsed}<{remaining}, {rate_fmt}]",
+        )
+
+
+def get_non_empty_lines_from_file(file_name: str) -> List[str]:
+    try:
+        with open(file_name, "r", encoding="utf-8") as file:
+            # Return a list with the non blank lines contained in the file.
+            return list(filter(None, (line.rstrip() for line in file)))
+    except Exception as e:
+        logger.error('Error during reading file "{0}": {1}'.format(file_name, e))
+        raise
+
+
+def get_nop_valid_op_codes() -> List[str]:
+    return get_non_empty_lines_from_file(
+        os.path.join(os.path.dirname(__file__), "resources", "op_code_for_nop.txt")
+    )
+
+def get_random_int(min_int: int, max_int: int) -> int:
+    return random.randint(min_int, max_int)
--- obfuscator_scripts/obfuscator-main.py
+++ obfuscator_scripts/obfuscator-main.py
@@ -1,11 +1,58 @@
 # imports here
 from .controller import Controller
-
+import helper
+import logging
+import re
+from typing import List
 
 class Obfuscation:
     def __init__(self):
         pass
+        # list to note which obfuscator has been used
+        self.used_obfuscators: List[str] = []
 
     # obfuscation methods below!!
+    def NOP_obfuscate(self):
+        self.logger.info('Running "{0}" obfuscator'.format(self.__class__.__name__))
+
+        try:
+            op_codes = helper.get_nop_valid_op_codes()
+            pattern = re.compile(r"\s+(?P<op_code>\S+)")
+
+            for smali_file in helper.show_list_progress(
+                self.get_smali_files(),
+                interactive=self.interactive,
+                description='Inserting "nop" instructions in smali files',
+            ):
+                self.logger.debug(
+                    'Inserting "nop" instructions in file "{0}"'.format(smali_file)
+                )
+                with helper.inplace_edit_file(smali_file) as (in_file, out_file):
+                    for line in in_file:
+
+                        # Print original instruction.
+                        out_file.write(line)
+
+                        # Check if this line contains an op code at the beginning
+                        # of the string.
+                        match = pattern.match(line)
+                        if match:
+                            op_code = match.group("op_code")
+                            # If this is a valid op code, insert some nop instructions
+                            # after it.
+                            if op_code in op_codes:
+                                nop_count = helper.get_random_int(1, 5)
+                                out_file.write("\tnop\n" * nop_count)
+
+        except Exception as e:
+            self.logger.error(
+                'Error during execution of "{0}" obfuscator: {1}'.format(
+                    self.__class__.__name__, e
+                )
+            )
+            raise
+
+        finally:
+            self.used_obfuscators.append(self.__class__.__name__)
 
 
