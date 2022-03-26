35ec82df338cc5f108bbd47b9537da9a0e028bc9
clamismagic <clamismagic@gmail.com>
2022-03-26 18:29:11 +0800

    code clean up

--- obfuscator_scripts/obfuscator_main.py
+++ obfuscator_scripts/obfuscator_main.py
@@ -1,6 +1,6 @@
 # imports here
 # from obfuscator_scripts import helper # pycharm
-import helper # vscode
+import helper  # vscode
 import re
 import random
 from typing import List, Dict, Match
@@ -10,13 +10,14 @@ class Obfuscator:
     def __init__(self):
         # TODO: implement checklist for obfuscator selection
         self.op_codes = helper.get_nop_op_codes()
-        self.pattern = re.compile(r"\s+(?P<op_code>\S+)") #can try r'^([ ]*)(?P<opCode>([^ ]+)
+        self.pattern = re.compile(r"\s+(?P<op_code>\S+)")  # can try r'^([ ]*)(?P<opCode>([^ ]+)
         self.locals_pattern = re.compile(r"\s+\.locals\s(?P<local_count>\d+)")
         self.label_pattern = re.compile(r"^[ ]{4}(?P<name>:.+)")
         self.class_pattern = re.compile(r"\.class.+?(?P<name>\S+?;)")
         self.method_patten = re.compile(r"\.method.+?(?P<method>\S+?)" + r"\((?P<args>\S*?)\)" + r"(?P<return>\S+)")
-        self.invoke_pattern = re.compile(r"^[ ]{4}(?P<invocation>invoke-\S+)\s"r"{(?P<variables>[0-9pv.,\s]*)},\s"r"(?P<class>\S+?)"r"->(?P<method>\S+?)"r"\((?P<args>\S*?)\)"r"(?P<return_type>\S+)")
-    
+        self.invoke_pattern = re.compile(
+            r"^[ ]{4}(?P<invocation>invoke-\S+)\s"r"{(?P<variables>[0-9pv.,\s]*)},\s"r"(?P<class>\S+?)"r"->(?P<method>\S+?)"r"\((?P<args>\S*?)\)"r"(?P<return_type>\S+)")
+
     def nop_obfuscator(self, smali_file: str):
         try:
             print(
@@ -36,7 +37,7 @@ class Obfuscator:
                         # If this is a valid op code, insert some nop instructions
                         # after it.
                         if op_code in self.op_codes:
-                            nop_count = helper.get_random_int(1, 5) #Randomize the number of nop(s)
+                            nop_count = helper.get_random_int(1, 5)  # Randomize the number of nop(s)
                             out_file.write("\tnop\n" * nop_count)
 
         except Exception as e:
@@ -91,7 +92,7 @@ class Obfuscator:
             )
             raise
 
-    def junk_method(self, smali_file: str): #abit useless
+    def junk_method(self, smali_file: str):
         try:
             print(
                 'Junk methods in file "{0}"'.format(smali_file)
@@ -100,17 +101,17 @@ class Obfuscator:
             with helper.inplace_edit_file(smali_file) as (in_file, out_file):
                 for line in in_file:
                     out_file.write(line)
-                    if re.search(r'^([ ]*?)# direct methods', line) is not None:  # At the top of the direct methods section
+                    if re.search(r'^([ ]*?)# direct methods',
+                                 line) is not None:  # At the top of the direct methods section
                         out_file.write(helper.get_junk_method("junkMethod.txt"))  # add the junk method
         except Exception as e:
             print(
-            'Error during execution of method renaming: {0}'.format(e)
+                'Error during execution of method renaming: {0}'.format(e)
             )
             raise
 
     def opaque_predicate(self, smali_file: str):
         try:
-            
             print(
                 'Inserting opaque predicate expressions in file "{0}"'.format(smali_file)
             )
@@ -121,7 +122,6 @@ class Obfuscator:
             pass_local: bool = False
             start_str: str = ""
             end_str: str = ""
-            temp_str: str = ""
 
             with helper.inplace_edit_file(smali_file) as (in_file, out_file):
                 for line in in_file:
@@ -130,15 +130,15 @@ class Obfuscator:
                     if not contains_label:
                         if self.label_pattern.match(line):
                             contains_label = True
-                    
+
                     # checks if the method allows for/ require instructions
                     # i.e. abstract, constructor or native methods
                     if (
-                        line.startswith(".method ")
-                        and "abstract" not in line
-                        and "native" not in line
-                        and "constructor" not in line
-                        and not in_method
+                            line.startswith(".method ")
+                            and "abstract" not in line
+                            and "native" not in line
+                            and "constructor" not in line
+                            and not in_method
                     ):
                         out_file.write(line)
                         in_method = True
@@ -158,7 +158,7 @@ class Obfuscator:
                             # if this a method without injection
                             # write a list containing original lines of code
                             out_file.writelines(method_wolabels)
-                        
+
                         # reset variables
                         out_file.write(line)
                         in_method = False
@@ -173,7 +173,7 @@ class Obfuscator:
                         # no additional lines will be added
                         method_wlabels.append(line)
                         method_wolabels.append(line)
-                    
+
                         # to inject the fake branch and its variables right after ".locals x"
                         match = self.locals_pattern.match(line)
                         if match and int(match.group("local_count")) >= 2:
@@ -182,11 +182,11 @@ class Obfuscator:
                             v0 = v1 = 0x0
 
                             # to make sure predicates are really opaque all the time
-                            # to prevent sitautions like [(6+3)%3 <= 0]
+                            # to prevent situations like [(6+3)%3 <= 0]
                             while v0 == v1:
                                 v0 = hex(helper.get_random_int(5, 32))
                                 v1 = hex(helper.get_random_int(7, 32))
-                         
+
                             start_str = helper.get_random_string(16)
                             end_str = helper.get_random_string(16)
                             temp_str = helper.get_random_string(16)
@@ -200,145 +200,150 @@ class Obfuscator:
                             method_wlabels.append("    goto/32 :{0}\n\n".format(end_str))
                             method_wlabels.append("    :{0}\n\n".format(temp_str))
                             method_wlabels.append("    :{0}\n".format(start_str))
-
-                            temp_str = ""
                     else:
                         out_file.write(line)
 
         except Exception as e:
             print(
-            'Error during execution of opaque predicate expressions: {0}'.format(e)
+                'Error during execution of opaque predicate expressions: {0}'.format(e)
             )
             raise
 
-
-    #Random Android Manifest line sequence
-    def rand_manifest(self, AndroidManifest: str):
+    # Random Android Manifest line sequence
+    def rand_manifest(self, android_manifest: str):
         try:
             print(
-                'Inserting "randomManifest" instructions in file "{0}"'.format(AndroidManifest)
+                'Inserting "randomManifest" instructions in file "{0}"'.format(android_manifest)
             )
-            
-            Uses = []               #This list is to store line that start with "<uses"
-            Application = []        #This list is to store lines that met certain critiria and is also a child in the <Application> parent
-            appLineNum = []         #Works together with Application list by storing the line number
-            app_start = 0           #Initilise app_start
-            app_stop = 0            #Initilise app_stop
-            lineNumberList = []     #Works together with Uses list by storing the line number
-            lineNumber = 0          #Initilise lineNumber
 
-            #Get file path and open file to read and write
-            with helper.inplace_edit_file(AndroidManifest) as (in_file, out_file):
+            # This list is to store line that start with "<uses"
+            uses = []
+            # This list is to store lines that met certain criteria and is also a child in the <Application> parent
+            application = []
+            # Works together with application list by storing the line number
+            app_line_num = []
+            # Initialise app_start
+            app_start = 0
+            # Initialise app_stop
+            app_stop = 0
+            # Works together with uses list by storing the line number
+            line_number_list = []
+            # Initialise line_number
+            line_number = 0
 
-                #1st loop to find line that begins with "uses-"
+            # Get file path and open file to read and write
+            with helper.inplace_edit_file(android_manifest) as (in_file, out_file):
+
+                # 1st loop to find line that begins with "uses-"
                 for line in in_file:
-                    
-                    #replace all android: to obfuscation:
-                    newLine = line.replace('android:', 'obfuscation:')
-                    line = newLine
 
-                    #Add line that start with     <uses-" and their line number to their respective list
-                    if (line.startswith("    <uses-")):
-                        Uses.append(line)
-                        lineNumberList.append(lineNumber)
+                    # replace all android: to obfuscation:
+                    new_line = line.replace('android:', 'obfuscation:')
+                    line = new_line
 
-                    #To get app_start for the next loop
-                    if (line.startswith("    <application")):
-                        app_start = lineNumber
-                    
-                    #To get app_stop for the next loop
-                    if (line.startswith("    </application>")):
-                        app_stop = lineNumber
+                    # Add line that start with     <uses-" and their line number to their respective list
+                    if line.startswith("    <uses-"):
+                        uses.append(line)
+                        line_number_list.append(line_number)
 
-                    lineNumber += 1
+                    # To get app_start for the next loop
+                    if line.startswith("    <application"):
+                        app_start = line_number
 
-                #Going back to start of file
+                    # To get app_stop for the next loop
+                    if line.startswith("    </application>"):
+                        app_stop = line_number
+
+                    line_number += 1
+
+                # Going back to start of file
                 in_file.seek(0)
-                lineNumber = 0
+                line_number = 0
 
-                #Loop 2 to find line that start with "        <" but not "        <activity" or "        <service" or "        </"
+                # Loop 2 to find line that start with "        <"
+                # but not "        <activity" or "        <service" or "        </"
                 for line in in_file:
 
-                    #replace all android: to obfuscation:
-                    newLine = line.replace('android:', 'obfuscation:')
-                    line = newLine
+                    # replace all android: to obfuscation:
+                    new_line = line.replace('android:', 'obfuscation:')
+                    line = new_line
 
-                    #If lineNumber is between <application> parent
-                    if ((lineNumber > app_start) and (lineNumber < app_stop)):
+                    # If line_number is between <application> parent
+                    if (line_number > app_start) and (line_number < app_stop):
                         if (
-                            (line.startswith("        <"))
-                            and not (line.startswith("        <activity"))
-                            and not (line.startswith("        <service"))
-                            and not (line.startswith("        </"))
+                                (line.startswith("        <"))
+                                and not (line.startswith("        <activity"))
+                                and not (line.startswith("        <service"))
+                                and not (line.startswith("        </"))
                         ):
-                            Application.append(line)
-                            appLineNum.append(lineNumber)
-                    
-                    lineNumber += 1
+                            application.append(line)
+                            app_line_num.append(line_number)
 
+                    line_number += 1
 
-                #Shuffle Uses list
-                random.shuffle(Uses)
+                # Shuffle uses list
+                random.shuffle(uses)
 
-                #Shuffle Application list
-                random.shuffle(Application)
+                # Shuffle application list
+                random.shuffle(application)
 
-                #Going back to start of file
+                # Going back to start of file
                 in_file.seek(0)
-                lineNumber = 0
+                line_number = 0
 
-                #Begins printing in out_file
+                # Begins printing in out_file
                 for line in in_file:
 
-                    #Remove the xml information in line 0
-                    if (lineNumber == 0):
+                    # Remove the xml information in line 0
+                    if line_number == 0:
                         line = re.sub('^.*<', '<', line)
 
-                    #replace all "android:" to "obfuscation:"
-                    newLine = line.replace('android:', 'obfuscation:')
-                    line = newLine.replace('xmlns:android', 'xmlns:obfuscation')
+                    # replace all "android:" to "obfuscation:"
+                    new_line = line.replace('android:', 'obfuscation:')
+                    line = new_line.replace('xmlns:android', 'xmlns:obfuscation')
 
-                    #This loop manage the line that is after the line that begines with <uses-
-                    #If lineNumberList list is not empty
-                    if lineNumberList:
+                    # This loop manage the line that is after the line that begines with <uses-
+                    # If line_number_list list is not empty
+                    if line_number_list:
 
-                        #If the current line nymber is not in the list, do not modify line
-                        if lineNumber != lineNumberList[0]:
+                        # If the current line nymber is not in the list, do not modify line
+                        if line_number != line_number_list[0]:
                             out_file.write(line)
-                        
-                        else:
-                            lineNumberList.pop(0)
-                            # Add the first element in the shuffled list of line that begin with "    <uses-"
-                            out_file.write(Uses[0])
 
-                            #If Uses list not empty, remove the first element
-                            if Uses:
-                                Uses.pop(0)
-                    
+                        else:
+                            line_number_list.pop(0)
+                            # Add the first element in the shuffled list of line that begin with "    <uses-"
+                            out_file.write(uses[0])
+
+                            # If uses list not empty, remove the first element
+                            if uses:
+                                uses.pop(0)
+
                     else:
 
-                        #This loop manage the line that is after the line <application
-                        #If appLineNum list not empty
-                        if appLineNum: 
+                        # This loop manage the line that is after the line <application
+                        # If app_line_num list not empty
+                        if app_line_num:
 
-                            #If the current line nymber is not in the list, do not modify line
-                            if lineNumber != appLineNum[0]:
+                            # if the current line nymber is not in the list, do not modify line
+                            if line_number != app_line_num[0]:
                                 out_file.write(line)
 
-                            #If line is in the list, replace it with the first element in Application list then remove the first element in appLineNum list
+                            # If line is in the list, replace it with the first element in application list,
+                            # remove the first element in app_line_num list
                             else:
-                                appLineNum.pop(0)
-                                out_file.write(Application[0])
+                                app_line_num.pop(0)
+                                out_file.write(application[0])
 
-                                #If Application list not empty, remove the first element
-                                if Application:
-                                    Application.pop(0)
+                                # If application list not empty, remove the first element
+                                if application:
+                                    application.pop(0)
 
                         else:
-                            # if both Appliaiton list and Uses list are empty, print the remaining lines
+                            # if both Application list and uses list are empty, print the remaining lines
                             out_file.write(line)
-                        
-                    lineNumber += 1
+
+                    line_number += 1
 
         except Exception as e:
             print(
--- ui_scripts/designer_ui.py
+++ ui_scripts/designer_ui.py
@@ -237,7 +237,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.compare_box.setLayout(QHBoxLayout())
         
         self.compare_box.setStyleSheet(qdarkstyle.load_stylesheet())
-        filelist = self.o.get_smali_files()
+        filelist = self.o.smali_files
         print(filelist[0])
         r = re.compile(f"\\\\{self.listWidget.currentItem().text()}")
         newlist = list(filter(r.match, filelist))
@@ -323,6 +323,7 @@ def main():
 
     # Create a Qt widget, which will be our window.
     window = MainWindow()
+    window.setWindowTitle("The Best Smali Obfuscator")
     window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
     # Start the event loop.
